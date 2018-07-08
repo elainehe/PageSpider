81e868253fc1cc7922a667ebc610c436c01019d0
kennethhe <kennethhe@yahoo.com>
2018-07-08 17:34:50 -0400

    initial commit

---
+++ utilities/url_utilities.py
@@ -0,0 +1,11 @@
+def load_urls_from_file(file_path: str):
+    #TODO: add the code need to read the text file with the url
+    pass
+
+def load_page(url: str):
+    #TODO: add the code
+    pass
+
+def scrape_page(page_contents:str):
+    #TODO: analyze the text
+    pass
